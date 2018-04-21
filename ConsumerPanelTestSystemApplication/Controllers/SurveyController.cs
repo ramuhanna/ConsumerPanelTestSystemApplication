c6af698df86e03725506f81eefb20273b15a927c
ramuhanna <ramuhanna@dah.edu.sa>
2018-04-21 12:56:06 +0300

    Create Survey Problem

--- ConsumerPanelTestSystemApplication/Controllers/SurveyController.cs
+++ ConsumerPanelTestSystemApplication/Controllers/SurveyController.cs
@@ -12,6 +12,11 @@ namespace ConsumerPanelTestSystemApplication.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
+        /// <summary>  
+        /// The Index action returns a list of questions from the database based on the selected QuestionnaireTypeId, which is the parameter used.
+        /// </summary>
+        /// <param name="id"></param> // The id used in the method is the QuestionnaireTypeId from the Questionnaire, in order to sort through the questions in the database.
+        /// <returns>Survey, Index view</returns>
         // GET: Survey
         public ActionResult Index(int? id)
         {
@@ -28,65 +33,76 @@ namespace ConsumerPanelTestSystemApplication.Controllers
                 new AnswerViewModel { Id = 1, Text= "Strongly Disagree"},
             };
 
-            var questions = from q in db.QuestionTypes
+            // Retreive QuestionId for questions with selected QuestionnaireTypeId
+            var questiontypes = from q in db.QuestionTypes
                                 where q.QuestionnaireTypeID == id
                                 select q.QuestionID;
 
-            //foreach (var item in questions)
-            //{
-            //    if (item.ResponseType == ResponseType.RadioButton)
-            //    {
-            //        var model = new QuestionViewModel()
-            //        {
-            //            Id = item.QuestionID,
-            //            QuestionText = item.QuestionText,
-            //            ResponseType = ResponseType.RadioButton,
-            //            PossibleAnswers = possibleAnswers
-            //        };
-            //    }
-            //    else if (item.ResponseType == ResponseType.RadioButton)
-            //    {
-            //        var model = new QuestionViewModel()
-            //        {
-            //            Id = item.QuestionID,
-            //            QuestionText = item.QuestionText,
-            //            ResponseType = ResponseType.TextBox,
-            //        };
-            //    };
-            //}
+            //// Retreive Questions based on questiontypes query???
+            //var questions = from q in db.Questions
+            //                where q.QuestionID == questiontypes
+            //                select q.QuestionId;
+
+
+            //Loop questions to retrieve relative response type.
+            foreach (var item in questions)
+            {
+                if (item.ResponseType == ResponseType.RadioButton)
+                {
+                    var model = new QuestionViewModel()
+                    {
+                        Id = item.QuestionID,
+                        QuestionText = item.QuestionText,
+                        ResponseType = ResponseType.RadioButton,
+                        PossibleAnswers = possibleAnswers
+                    };
+                }
+                else if (item.ResponseType == ResponseType.RadioButton)
+                {
+                    var model = new QuestionViewModel()
+                    {
+                        Id = item.QuestionID,
+                        QuestionText = item.QuestionText,
+                        ResponseType = ResponseType.TextBox,
+                    };
+                };
+            }
 
             var modell = new SurveyViewModel();
 
             return View();
         }
-        
-        // GET: Survey/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Survey/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Index(SurveyViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // Save the questions with selected answers from the VM to your database
+        //        foreach (var question in model.Questions)
+        //        {
+        //            question.Id,
+        //            question.SelectedAnswer,
+        //            question.Input
+        //            question.ResponseType
+        //        }
+
+        //        // Return to to your home
+        //        return RedirectToAction("Index", "Home");
+        //    }
+
+        //    // Issue with the model
+        //    return View(model);
+        //}
 
         // GET: Survey/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
+        
 
         // POST: Survey/Edit/5
         [HttpPost]
